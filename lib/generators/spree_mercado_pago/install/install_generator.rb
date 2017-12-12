f93421505a7bd7bea2b9f284ac7de7a5b8bdc6b7
ngelx <ngel@protonmail.com>
2017-12-12 14:56:50 -0300

    change in generator in order to be able to auto install migrations

--- lib/generators/spree_mercado_pago/install/install_generator.rb
+++ lib/generators/spree_mercado_pago/install/install_generator.rb
@@ -1,6 +1,8 @@
 module SpreeMercadoPago
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, type: :boolean, default: false
+      
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_mercado_pago\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_mercado_pago\n"
@@ -21,8 +23,8 @@ module SpreeMercadoPago
       end
 
       def run_migrations
-        res = ask 'Would you like to run the migrations now? [Y/n]'
-        if res == '' || res.casecmp('y').zero?
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+        if run_migrations
           run 'bundle exec rake db:migrate'
         else
           puts 'Skipping rake db:migrate, don\'t forget to run it!'
